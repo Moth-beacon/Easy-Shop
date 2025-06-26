cd3ae100b7601ce88ed7c9cae396a2fc69e9e012
Moth-beacon <gomeztimothy234@gmail.com>
2025-06-26 09:47:11 -0400

    Add simple comments to ShoppingCartDao interface

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,11 +4,15 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
+    // Get the cart for a specific user
     ShoppingCart getByUserId(int userId);
 
+    // Add a product to the user's cart
     void addProduct(int userId, int productId);
 
+    // Change how many of a product is in the cart
     void update(int userId, int productId, int quantity);
 
+    // Remove everything from the user's cart
     void clear(int userId);
 }
