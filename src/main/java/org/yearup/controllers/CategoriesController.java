85247c830e946591d62bbc8a57dde868a5ca4ee4
Moth-beacon <gomeztimothy234@gmail.com>
2025-06-26 09:00:38 -0400

    Start of major push starting with cat-controller.java class

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,66 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
+public class CategoriesController
+{
+    private final CategoryDao categoryDao;
+    private final ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao)
+    {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
+    @GetMapping
+    public List<Category> getAll()
+    {
+        return categoryDao.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable int id)
+    {
+        return categoryDao.getById(id);
+    }
+
+    @GetMapping("/{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        return productDao.getByCategoryId(categoryId);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public Category addCategory(@RequestBody Category category)
+    {
+        return categoryDao.create(category);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        categoryDao.update(id, category);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteCategory(@PathVariable int id)
+    {
+        categoryDao.delete(id);
+    }
+}
